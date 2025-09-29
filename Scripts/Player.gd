bb1752f5af15088a57ea481072d5dbaf9f68530c
Hamish Hillyer <GameVerseHamish@gmail.com>
2025-09-29 12:45:01 +0930

    Testing

---
+++ Scripts/Player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
+@export var LRMovement = false
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	if LRMovement == true:
+		var direction := Input.get_axis("Left", "Right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			
+	move_and_slide()
