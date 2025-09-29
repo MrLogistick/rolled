635c04617892876ccb14cd6961c657b74dc010ea
MrLogistick <logan.wurst10@gmail.com>
2025-09-29 21:39:56 +0930

    Update spawn_point.gd

--- Scripts/spawn_point.gd
+++ Scripts/spawn_point.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
 const GROUND = preload("res://Scenes/ground.tscn")
+const SPAWNPOINT: Vector2 = Vector2(800, 250)
 
 func _ready() -> void:
-	spawn_ground(Vector2(-800, 250))
+	spawn_ground(Vector2(-SPAWNPOINT.x, SPAWNPOINT.y))
 
 func spawn_ground(pos: Vector2) -> void:
 	var ground = GROUND.instantiate()
-	ground.spawnpoint = Vector2(800, 250)
+	ground.spawnpoint = SPAWNPOINT
 	ground.global_position = pos
 	ground.spawn_node = %SpawnPoint
 	add_child(ground)
