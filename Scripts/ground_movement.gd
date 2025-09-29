92f501a0216d116d0193f7accf7775ae5976ca35
MrLogistick <logan.wurst10@gmail.com>
2025-09-29 21:37:12 +0930

    Working Platforms

--- Scripts/ground_movement.gd
+++ Scripts/ground_movement.gd
@@ -1,19 +1,14 @@
 extends StaticBody2D
 
 @onready var end_platform: Node2D = $EndPlatform
-
 var spawn_node
-
 
 const PLATFORM_SIZE = 128
 var spawnpoint: Vector2
 var speed = 60
 var has_spawned = false
 
-
-
 func _process(delta: float) -> void:
-	spawnpoint = Vector2(800, 250)
 	if spawnpoint == Vector2.ZERO:
 		print("'spawnpoint' has not been set.")
 		return
@@ -23,6 +18,7 @@ func _process(delta: float) -> void:
 	var ep_position = end_platform.global_position
 	# spawns a new platform when the edge of the old platform reaches the edge
 	if ep_position.x < spawnpoint.x && has_spawned == false:
+		print(ep_position)
 		spawn_node.spawn_ground(Vector2(ep_position.x + PLATFORM_SIZE / 2, ep_position.y))
 		has_spawned = true
 	
--- Scripts/spawn_point.gd
+++ Scripts/spawn_point.gd
@@ -3,12 +3,11 @@ extends Node2D
 const GROUND = preload("res://Scenes/ground.tscn")
 
 func _ready() -> void:
-	spawn_ground(Vector2(-global_position.x, global_position.y))
+	spawn_ground(Vector2(-800, 250))
 
 func spawn_ground(pos: Vector2) -> void:
 	var ground = GROUND.instantiate()
+	ground.spawnpoint = Vector2(800, 250)
 	ground.global_position = pos
 	ground.spawn_node = %SpawnPoint
 	add_child(ground)
-	print(global_position)
-	ground.spawnpoint = global_position
