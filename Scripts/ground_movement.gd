eba4cb86bdc114a1a9cac170f6aa78625cd3736a
MrLogistick <logan.wurst10@gmail.com>
2025-09-29 21:41:13 +0930

    Update ground_movement.gd

--- Scripts/ground_movement.gd
+++ Scripts/ground_movement.gd
@@ -18,7 +18,6 @@ func _process(delta: float) -> void:
 	var ep_position = end_platform.global_position
 	# spawns a new platform when the edge of the old platform reaches the edge
 	if ep_position.x < spawnpoint.x && has_spawned == false:
-		print(ep_position)
 		spawn_node.spawn_ground(Vector2(ep_position.x + PLATFORM_SIZE / 2, ep_position.y))
 		has_spawned = true
 	
