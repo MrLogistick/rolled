927c77d63802ebbe7c8bcbda5058316059847f66
MrLogistick <logan.wurst10@gmail.com>
2025-10-07 13:07:54 +1030

    Errors Push

--- Scripts/ground_movement.gd
+++ Scripts/ground_movement.gd
@@ -2,16 +2,13 @@ extends StaticBody2D
 
 @onready var end_platform: Node2D = $EndPlatform
 var spawn_node
-
-@onready var SpawnPointNode: Node2D = %SpawnPoint
-
 @onready var Player: CharacterBody2D = %Player
 
 var Stop = false
 
-const PLATFORM_SIZE: float = 128
+const PLATFORM_SIZE: float = 64
 var spawnpoint: Vector2
-var speed = 640
+var speed = 320
 var has_spawned = false
 
 func _process(delta: float) -> void:
@@ -19,13 +16,13 @@ func _process(delta: float) -> void:
 		print("'spawnpoint' has not been set.")
 		return
 	
-	position += -transform.x.normalized() * speed * delta
+	global_position -= Vector2(speed, speed / 2) * delta
 	
 	var ep_position = end_platform.global_position
 	# spawns a new platform when the edge of the old platform reaches the edge
 	if ep_position.x < spawnpoint.x && has_spawned == false:
-		# spawn_node.spawn_ground(Vector2(ep_position.x + PLATFORM_SIZE / 2, ep_position.y))
-		spawn_node.spawn_ground(Vector2(ep_position + transform.x * PLATFORM_SIZE / 2))
+		spawn_node.spawn_ground(Vector2(ep_position.x + PLATFORM_SIZE / 2, ep_position.y))
+		# spawn_node.spawn_ground(Vector2(ep_position + transform.x * PLATFORM_SIZE / 2))
 		has_spawned = true
 	
 	# destroys platform once out of range
@@ -35,5 +32,4 @@ func _process(delta: float) -> void:
 
 func On_Spike_Trap_Area_Entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		var SpawningNode = $".".get_parent()
-		SpawningNode.Death()
+		spawn_node.Death()
